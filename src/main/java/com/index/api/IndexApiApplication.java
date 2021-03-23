5d7442ad7de179d2579cec8ca3dda8fdfb943967
YuraBondarenko <yuriybondarenko2001@gmail.com>
2021-03-23 22:49:14 +0200

    first commit

---
+++ src/main/java/com/index/api/IndexApiApplication.java
@@ -0,0 +1,12 @@
+package com.index.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndexApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IndexApiApplication.class, args);
+    }
+
+}
